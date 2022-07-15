3a6d8643ffe4c87592450a114ca52ba520b3b992
Thiago Attico da Silva <thiago.attico@itlab.com.br>
2022-07-14 21:14:56 -0300

    Selecionando uma tarefa

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from './botao.module.scss';
 
-class Botao extends React.Component {
+class Botao extends React.Component<any, any> {
   render() {
+    const { type = "button" } = this.props;
     return (
-      <button className={style.botao}>
+      <button type={type} className={style.botao}>
         {this.props.children}
       </button>
     )
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
+import { ITarefa } from "../../types/ITarefa";
 import Item from "./Item";
-import style from "./Lista.module.scss"
+import style from "./Lista.module.scss";
 
-function Lista(){
-    const [tarefas, setTarefas] = useState([{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'Typescript',
-        tempo: '03:00:00'
-    }]);
-       
+interface Props{
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+
+function Lista({tarefas, selecionaTarefa} : Props){
     return(
         <aside className={style.listaTarefas}>
-            <h2 onClick={() => {
-                setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}])
-            }}> Estudos do dia</h2>
+            <h2> Estudos do dia</h2>
             <ul>
-                {tarefas.map((item, index) =>(
+                {tarefas.map(item =>(
                     <Item 
-                        key={index}
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
                         {...item}
                     />
                 ))}
